ac67a81d720f4416e2dba69b26fae5a66abe3147
S29 <nots29vv@gmail.com>
2024-03-30 15:25:29 +0100

    added ogImgNoQueryParams Landing frame

---
+++ app/og/landing/route.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable @next/next/no-img-element  */
+/* eslint-disable jsx-ally/alt-text */
+// @ts-nocheck
+import { ImageResponse } from 'next/og'
+
+export const runtime = 'edge';
+
+export async function GET(req: Request) {
+    const { searchParams } = new URL(req.url);
+    if (searchParams.size != 0) {
+        
+    }
+    
+   return await ogImgNoQueryParams()
+}
+
+async function ogImgNoQueryParams(): ImageResponse {
+    const ImgNoQueryParams = await fetch(new URL('../../../public/ogImgNoQueryParams.png', import.meta.url)).then(
+        (res) => res.arrayBuffer(),
+      );
+    return new ImageResponse(
+        (
+            <div style={{position: 'relative', display: 'flex'}}>
+                      <img tw='h-full' src={ImgNoQueryParams}/>
+            </div>
+        )
+    )
+}
+
