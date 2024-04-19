5a448e6f3dabb56a64b619bb773bb5e2f07a7301
S29 <nots29vv@gmail.com>
2024-04-19 21:06:57 +0100

    modified page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -71,7 +71,7 @@ export async function generateMetadata(
     postUrl += "/api"
   }
 
-  if (Object.keys(searchParams).length == 0) {
+  if (!searchParams) {
     let buttonLabel = 'Invalid Games';
     buttons = [
       {label: buttonLabel, action: 'post'}
