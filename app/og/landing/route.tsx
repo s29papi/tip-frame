09a96fe38278b78ab91bbd429b810e1fb402df1b
S29 <nots29vv@gmail.com>
2024-04-20 23:36:05 +0100

    modified page.tsx

--- app/og/landing/route.tsx
+++ app/og/landing/route.tsx
@@ -7,7 +7,8 @@ export const runtime = 'edge';
 export async function GET(req: Request) {
     const { searchParams } = new URL(req.url);
     if (searchParams.size != 0) {
-        return await ogImgNoQueryParams()
+        const stakeAmount = searchParams.get('stakeAmount')
+        return await ogImgWithQueryParams(stakeAmount)
     }
     
    return await ogImgNoQueryParams()
@@ -26,7 +27,36 @@ async function ogImgNoQueryParams(): ImageResponse {
     )
 }
 
-async function ogImgWithQueryParams(): ImageResponse {
-
+async function ogImgWithQueryParams(stakeAmountWithToken: string): ImageResponse {
+    const ogImgWithQueryParams = await fetch(new URL('../../../public/ogImgWithQueryParams.png', import.meta.url)).then(
+        (res) => res.arrayBuffer(),
+      );
+      const fontData = await fetch(
+        new URL('../../../assets/TitilliumWeb-Italic.ttf', import.meta.url),
+      ).then((res) => res.arrayBuffer());
+    return new ImageResponse(
+        (
+            <div style={{position: 'relative', display: 'flex'}}>
+                      <img tw='h-full' alt='image used for no query params' src={ogImgWithQueryParams}/>
+                        <div 
+                            style={{display: 'flex', position: 'absolute', top: '65%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center', color: '#66757F', fontSize: '72px', fontWeight: '1200', fontFamily: '"Titillium Web"'}}
+                        > 
+                            <span>
+                                STAKE {stakeAmountWithToken}
+                            </span>
+                        </div>
+            </div>
+        ),
+        {
+            fonts: [
+                {
+                    name: 'Titillium Web',
+                    data: fontData,
+                    style: 'italic',
+                    weight: 900
+                }
+            ]
+        }
+    )
 }
 
