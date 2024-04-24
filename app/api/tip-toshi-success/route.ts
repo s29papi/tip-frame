165d63c4badda5b3d83cfe5c78aaad1cdec0cd36
S29 <nots29vv@gmail.com>
2024-04-24 18:54:04 +0100

    image commit

--- app/api/tip-toshi-success/route.ts
+++ app/api/tip-toshi-success/route.ts
@@ -2,23 +2,25 @@ import { FrameRequest, getFrameMessage } from '@coinbase/onchainkit';
 import { NextRequest, NextResponse } from 'next/server';
 
 
-
+const FRAMES_URL = process.env.FRAMES_URL || "https://tip-frame.vercel.app"
+const imageUrl = new URL("/og/tipPage", FRAMES_URL).href
+const postUrl = new URL("/", FRAMES_URL).href
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   const body: FrameRequest = await req.json();
+  const { isValid } = await getFrameMessage(body);
 
-  const framesUrl = "https://tip-frame.vercel.app"; 
-  let imageUrl = new URL(`/og/tipPage`, framesUrl).href
-  let postUrl = new URL(`/`, framesUrl).href
-
+  if (!isValid) {
+    return new NextResponse('Message not valid', { status: 500 });
+  }
 
     return new NextResponse(`<!DOCTYPE html><html><head>
-          <title>Start My Match</title>
-          <meta property="fc:frame" content="vNext" />        
-          <meta property="fc:frame:image" content="${imageUrl}"/>
-          <meta property="fc:frame:button:1" content="TIP Status" />
-          <meta property="fc:frame:button:1:action" content="post"/>
-          <meta property="fc:frame:post_url" content="${postUrl}"/>
-      </head></html>`);
+            <title>Start My Match</title>
+            <meta property="fc:frame" content="vNext" />        
+            <meta property="fc:frame:image" content="${imageUrl}"/>
+            <meta property="fc:frame:button:1" content="TIP Statussssssssssssssssssssssssss" />
+            <meta property="fc:frame:button:1:action" content="post"/>
+            <meta property="fc:frame:post_url" content="${postUrl}"/>
+        </head></html>`);
 }
 
 export async function POST(req: NextRequest): Promise<Response> {
