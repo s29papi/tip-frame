53034ec9722ec26383ac15f68f9523492be2f507
S29 <nots29vv@gmail.com>
2024-04-26 11:03:24 +0100

    modified toshipay

--- app/api/tip-toshi-tx/route.ts
+++ app/api/tip-toshi-tx/route.ts
@@ -3,25 +3,34 @@ import { NextRequest, NextResponse } from 'next/server';
 import { encodeFunctionData, formatEther, parseEther, parseUnits  } from 'viem';
 import { base } from 'viem/chains';
 import type { FrameTransactionResponse } from '@coinbase/onchainkit/frame';
-
+import { getFarcasterUserAddress } from '@coinbase/onchainkit/farcaster';
+ 
 import ToshiBaseABI from "../../contracts-utils/toshi-abi";
 import { TOSHI_BASE_CONTRACT_ADDR } from '../../config';
-import { RECIEVER_ADDR } from '../../config';
+// import { RECIEVER_ADDR } from '../../config';
 
 async function getResponse(req: NextRequest): Promise<NextResponse | Response> {
     const body: FrameRequest = await req.json();
-    const { isValid } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
+    const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
   
     if (!isValid) {
       return new NextResponse('Message not valid', { status: 500 });
     }
 
-    const data = encodeFunctionData({
+    const farcasterUserAddress = await getFarcasterUserAddress(body.untrustedData.fid, {
+      neynarApiKey: 'MY_NEYNAR_API_KEY', 
+    });
+    let recAddr = farcasterUserAddress?.verifiedAddresses
+    let data;
+
+    if (recAddr) {
+     data = encodeFunctionData({
         abi: ToshiBaseABI,
         functionName: 'transfer',
-        args: [RECIEVER_ADDR, parseUnits('1', 18) ], 
+        args: [`0x${recAddr.slice(2)}`, parseUnits('1', 18) ], 
       });
-
+    }
+  
     const txData: FrameTransactionResponse = {
         chainId: `eip155:${base.id}`,
         method: 'eth_sendTransaction',
